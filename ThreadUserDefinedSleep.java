72dbfdfe03a2c6693f6c2b0b3d8f1b6d81518ce6
Mayuri Mhaske <mayurimhaske24@gmail.com>
2025-11-08 18:29:09 +0530

    Programs Files

---
+++ ThreadUserDefinedSleep.java
@@ -0,0 +1,37 @@
+//code to demonstrates thread scheduling
+class Demo extends Thread{
+    public void run()
+    {
+        int i = 0;
+        for(i = 1; i<=5; i++){
+            System.out.println("Inside run method of: "+Thread.currentThread().getName());
+        }
+    }
+}
+class ThreadUserDefinedSleep
+{
+    public static void main(String[ ] a) 
+    {
+        System.out.println("Inside main thread");
+        Demo dobj1 = new Demo();
+        Demo dobj2 = new Demo();
+
+        dobj1.setName("First");
+        dobj2.setName("Second");
+
+        try
+        {
+             dobj1.start();
+             dobj1.join();
+             
+             dobj2.start();
+             dobj2.join();
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+
+        System.out.println("End of main thread");
+    }
+}
